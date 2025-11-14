07c56c95e355b4ccfaa125858e2096d1f68e816a
Gui-diegues <guilhermediegues2006@gmail.com>
2025-11-13 21:08:35 -0300

    Editando arquivos recem adicionados

---
+++ rodape.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <style>
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+    </style>
+</head>
+<body>
+    <footer style="background-color: #0a73dcff; color: white; text-align: center; padding: 10px; margin-top: 20px;">
+    <p>&copy; 2025 - Json Calçados. Todos os direitos reservados.</p>
+</footer>
+
+</body>
+</html>
---
+++ topo.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+     <style>
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: Arial, Helvetica, sans-serif;
+
+        }
+    </style>
+</head>
+<body>
+    <header style="background-color: #0a73dcff; color: white; padding: 20px; text-align: center;">
+    <h1>Fone Tech</h1>
+    <p>Bem-vindo a Json Calçados </p>
+</header>
+</body>
+</html>
