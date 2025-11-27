a98340af86e0c899f843d86e6db0a7a51e93558b
Gui-diegues <guilhermediegues2006@gmail.com>
2025-11-27 10:31:51 -0300

    Fix no painel admin

--- admin/topoadmin.php
+++ admin/topoadmin.php
@@ -1,14 +1,14 @@
 <style>
-    *{
-        margin: 0;
-        padding: 0;
-        font-family: Arial, Helvetica, sans-serif;
+*{
+    margin: 0;
+    padding: 0;
+    font-family: Arial, Helvetica, sans-serif;
 
-    }
-h2{
+}
+.txtopo{
     text-align: center;
 }
-div{
+.topo{
     background-color: #4A90E2;
     color: white;
     padding: 50px;
@@ -16,6 +16,6 @@ div{
 }
 
 </style>
-<Div>
-    <h1>Painel Administrativo</h1>
+<Div class="topo">
+    <h1 class="txtopo">Painel Administrativo</h1>
 </Div>
--- admin/vendas_admin.php
+++ admin/vendas_admin.php
@@ -6,9 +6,9 @@
     font-size: 28px;
 }
 .forms {
-    width: 400px;
+    display: flex;            /* usa flexbox */
     flex-wrap: wrap;          /* permite quebrar para a próxima linha */
-    gap: 16px;                /* espaço entre as caixas */
+    gap: 12px;                /* espaço entre as caixas */
     justify-content: center;  /* centraliza as caixas na linha */
     padding: 20px;
     box-sizing: border-box;
@@ -16,19 +16,28 @@
 
   /* cada caixa */
   .item {
-    width: 300px;          /* não cresce, pode encolher, base 300px */
+    
+    width: 250px;          /* não cresce, pode encolher, base 300px */
     box-sizing: border-box;
     border: 1px solid #ddd;
-    padding: 12px;
-    background: #fff;
+    padding: 15px;
+    background-color: #fff;
     border-radius: 6px;
   }
 .editar {
-    background-color: #007bff;
+    background-color: #3491f5ff;
+    border: 1px solid #0f0f0fff;
+    color: white;
+    padding: 1px 5px;
+    margin: 1px;
 }
 
 .excluir {
-    background-color: #dc3545;
+    background-color: #f14153ff;
+    border: 1px solid #0f0f0fff;
+    color: white;
+    padding: 1px 5px;
+    margin: 1px;
 }
 </style>
 
@@ -42,32 +51,26 @@
 
     echo"<h2 class='text'>Lista de produtos</h2>";
 
+    echo "<div class='forms'>";
+
     if (mysqli_num_rows($resultado) > 0){
         while($dados = mysqli_fetch_array($resultado)) {
             
-            echo "<div class='forms'>";
+            
             echo "<div class='item'>";
+
             echo "ID: " . $dados["id"] . "<br>";
-            echo "</div>";
-
-            echo "<div class='item'>";
             echo "Produto: " . $dados["produto"] . "<br>";
-            echo "</div>";
-
-            echo "<div class='item'>";
             echo "Quantidade: " . $dados["quantidade"] . "<br>";
-            echo "</div>";
-
-            echo "<div class='item'>";
             echo "Valor: " . $dados["valor"] . "<br>";
-            echo "</div>";
-            
-        echo "</div>";
-            echo "<a class='editar' href='?pg=vendas_form_alterar&id=$dados[id]'>Editar</a>";
-            echo " | <a class='excluir'href='?pg=vendas_excluir&id=$dados[id]'>Excluir</a>";
-            
 
+
+            echo "<a class='editar' href='?pg=vendas_form_alterar&id=$dados[id]'>Editar</a>";
+            echo " | <a class='excluir'href='?pg=vendas_excluir&id=$dados[id]'>Excluir</a>";   
+        echo "</div>";
+    
         }
+    echo "</div>";
     }else{
         echo "<h2>Nenhum produto cadastrado</h2>";
         echo "<a href='?pg=index'>Voltar</a>";
