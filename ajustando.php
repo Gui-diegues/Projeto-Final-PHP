47a0a729aec40d2d808a399da9428781f77e8e5b
Yrllan <yrllanriquelme.200@gmail.com>
2025-12-03 19:31:50 -0300

    ajustes.php
    This file contains customer testimonials and ratings for products, structured in HTML.

---
+++ ajustando.php
@@ -0,0 +1,44 @@
+ <h2 class="title">Como foram as experiências de nossos últimos clientes</h2>
+
+        <div class="row">
+
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>"Comprei esse tênis para usar no trajeto da faculdade, já que eu ando bastante lá, e eles são muito bons, confortáveis, amortecedor é bom e são fáceis de colocar ou tirar. Vale super a pena."</p>
+
+                <div class="rating">
+                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                </div>
+
+                <img src="https://scontent.fjpa1-1.fna.fbcdn.net/v/t39.30808-6/510974862_24180971788182542_4597947759565162940_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=a5f93a&_nc_ohc=AHYBt6gixQkQ7kNvwHNHetp&_nc_oc=AdnBUcho99Y81yHla4L1jM0Es_B4zbvKTMs_evIiPXq_-4y-etrjPF0BKcuAvFKn0DK5utrbzFaHLLLvpt3QdueI&_nc_zt=23&_nc_ht=scontent.fjpa1-1.fna&_nc_gid=hcT8fQWEYcvC6zxB32XPAw&oh=00_AflEMyUF5N46fZqsi8CADqhHzN-lvBat_6YHHICfu7rI2A&oe=6935F5D0" alt="Julia">
+                <h3>Ionara Sarai</h3>
+            </div>
+
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>"O sapato é extremamente confortável. O material é bom, o custo benefício é ótimo e o tamanho indicado na descrição bateu certinho. Meus treinos melhoraram 100%."</p>
+
+                <div class="rating">
+                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i>
+                </div>
+
+                <img src="https://media.licdn.com/dms/image/v2/D4D03AQGObCSg2zjQuA/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1687782388636?e=2147483647&v=beta&t=EMyyG9qmIPnB_eAVDvLoOEd85u0R1QpnBpBoECvQ9VA" alt="Ricardo">
+                <h3>Daniel Brandão</h3>
+            </div>
+
+            <div class="col-3">
+                <i class="fa fa-quote-left"></i>
+                <p>"Fico deveras impressionado com o esmero deste calçado, que remete aos áureos tempos dos velhos mestres sapateiros. Recebam meus sinceros cumprimentos pela fidalguia do produto"</p>
+
+                <div class="rating">
+                    <i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+                </div>
+
+                <img src="https://media.licdn.com/dms/image/v2/C4E03AQGKiRvuHb-V6w/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1627990737248?e=2147483647&v=beta&t=Nt4WQVVo1PM0G2r10ZyyuqvNISEnp-pgOXR6RiYJ2vA" alt="Roberto">
+                <h3>Jadilson Paiva</h3>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
